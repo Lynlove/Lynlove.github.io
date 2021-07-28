2e38d99013503affe53671508b005fd7ea748b9e
Lynlove <3228562873@qq.com>
2021-07-28 15:22:27 +0800

    Site updated: 2021-07-28 15:22:27

---
+++ dist/music.js
@@ -0,0 +1,39 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+
+        {
+            name: 'lover',
+            artist: '年年的老婆Taylor',
+            url: 'http://music.163.com/song/media/outer/url?id=1403250178.mp3',
+            cover: 'https://p2.music.126.net/Kn3_QaCQyCXYzrpYZOV70w==/109951164485490984.jpg?',
+        },
+        {
+            name: "你一定要幸福",
+            artist: '简弘亦',
+            url: 'http://music.163.com/song/media/outer/url?id=439121995.mp3',
+            cover: 'https://p1.music.126.net/CGhCWp8HV5IyD-FfEWtWsw==/18613632347661213.jpg?',
+        },
+        {
+            name: '囍（Chinese Wedding）',
+            artist: '葛东琪',
+            url: 'http://music.163.com/song/media/outer/url?id=1303289043.mp3',
+            cover: 'http://p1.music.126.net/H3Q3SeMVtuHvHY2uyaQdOw==/109951163472855051.jpg?'
+        },
+        {
+            name: '回到夏天',
+            artist: '傲七爷 / 小田音乐社',
+            url: 'http://music.163.com/song/media/outer/url?id=1449678888.mp3',
+            cover: 'https://p1.music.126.net/6DtmsO-P4jyJPLjnGa8Ytg==/109951165004114065.jpg?',
+        },
+        {
+            name: 'secret base ~君がくれたもの~',
+            artist: '茅野愛衣 / 戸松遥 / 早見沙織',
+            url: 'http://music.163.com/song/media/outer/url?id=33911781.mp3',
+            cover: 'http://p1.music.126.net/daZcHVIJicL3wXJWMIjAng==/7926379325753633.jpg?',
+        }
+      
+    ]
+});
